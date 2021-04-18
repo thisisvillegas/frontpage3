7f78ac0eabcabef0cb9fb15374f34d942d058c09
Andres <thisisvillegas@gmail.com>
2021-04-17 21:11:41 -0500

    updated clock format

--- src/components/clock.component.js
+++ src/components/clock.component.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Clock from 'react-live-clock';
 
 export default class MyComponent extends Component {
-	render() {
-		return (
-			<div className="time">
-				<Clock format={'MMM DD YYYY h:mm:ss a'} ticking={true} timezone={'US/Central'} />
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="time">
+                <Clock
+                    format={'MMM DD YYYY'}
+                    ticking={true}
+                    timezone={'US/Central'}
+                />
+                <br />
+                <Clock
+                    format={'h:mm:ss a'}
+                    ticking={true}
+                    timezone={'US/Central'}
+                />
+            </div>
+        );
+    }
 }
